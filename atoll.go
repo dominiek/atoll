f8762429dd99d05d90a3bf4ef18fc7286738e0a5
Dominiek Ter Heide <dominiek@bottlenose.com>
2015-09-28 22:12:11 -0700

    Updated Docker example

--- atoll.go
+++ atoll.go
@@ -18,7 +18,7 @@ func main() {
   app := cli.NewApp()
   app.Name = "atoll"
   app.Usage = "Monitoring agent for Atoll"
-  app.Version = "0.0.1"
+  app.Version = "0.1.2"
   app.Flags = []cli.Flag {
     cli.StringFlag{
       Name: "config, c",
